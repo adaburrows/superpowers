746d874145f780a4305b777d15df983f6afa619d
Jill Burrows <jill@puppetlabs.com>
2012-11-01 20:05:07 -0700

    Finished initial rewrite. The code should make more sense now.

--- Superpowers/src/com/adaburrows/superpowers/MagicOverlay.java
+++ Superpowers/src/com/adaburrows/superpowers/MagicOverlay.java
@@ -2,7 +2,6 @@ package com.adaburrows.superpowers;
 
 import java.io.IOException;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
@@ -10,15 +9,13 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.hardware.Camera;
-import android.hardware.Camera.PreviewCallback;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.ViewGroup.LayoutParams;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.media.MediaRecorder.AudioSource;
+import android.media.audiofx.Visualizer;
 
 /*
  * This is probably mostly going away, in favor of drawing data from audio.
@@ -35,251 +32,66 @@ class MagicOverlay extends View {
   Paint mPaintRed;
   Paint mPaintGreen;
   Paint mPaintBlue;
-  byte[] mYUVData;
-  int[] mRGBData;
   int mImageWidth, mImageHeight;
-  int[] mRedHistogram;
-  int[] mGreenHistogram;
-  int[] mBlueHistogram;
-  double[] mBinSquared;
 
   // Constructor
-  public MagicOverlay(Context context) {
+  public MagicOverlay(Context context, Visualizer visualizer) {
     super(context);
         
     mPaintBlack = new Paint();
     mPaintBlack.setStyle(Paint.Style.FILL);
     mPaintBlack.setColor(Color.BLACK);
+    mPaintBlack.setAlpha(128);
     mPaintBlack.setTextSize(25);
         
     mPaintYellow = new Paint();
     mPaintYellow.setStyle(Paint.Style.FILL);
     mPaintYellow.setColor(Color.YELLOW);
+    mPaintYellow.setAlpha(128);
     mPaintYellow.setTextSize(25);
         
     mPaintRed = new Paint();
     mPaintRed.setStyle(Paint.Style.FILL);
     mPaintRed.setColor(Color.RED);
+    mPaintRed.setAlpha(128);
     mPaintRed.setTextSize(25);
         
     mPaintGreen = new Paint();
     mPaintGreen.setStyle(Paint.Style.FILL);
     mPaintGreen.setColor(Color.GREEN);
+    mPaintGreen.setAlpha(128);
     mPaintGreen.setTextSize(25);
         
     mPaintBlue = new Paint();
     mPaintBlue.setStyle(Paint.Style.FILL);
     mPaintBlue.setColor(Color.BLUE);
+    mPaintBlue.setAlpha(128);
     mPaintBlue.setTextSize(25);
         
     mBitmap = null;
-    mYUVData = null;
-    mRGBData = null;
-    mRedHistogram = new int[256];
-    mGreenHistogram = new int[256];
-    mBlueHistogram = new int[256];
-    mBinSquared = new double[256];
-    for (int bin = 0; bin < 256; bin++)
-    {
-      mBinSquared[bin] = ((double)bin) * bin;
-    }
   }
 
   @Override
   protected void onDraw(Canvas canvas) {
     // Draw if we have a bitmap to draw on
-    if (mBitmap != null) {
+    //if (mBitmap != null) {
       int canvasWidth = canvas.getWidth();
       int canvasHeight = canvas.getHeight();
       int newImageWidth = canvasWidth;
       int newImageHeight = canvasHeight;
       int marginWidth = (canvasWidth - newImageWidth)/2;
 
-      // Convert from YUV to RGB
-      decodeYUV420SP(mRGBData, mYUVData, mImageWidth, mImageHeight);
-
-      // Calculate histogram
-      calculateIntensityHistogram(mRGBData, mRedHistogram, 
-        mImageWidth, mImageHeight, 0);
-      calculateIntensityHistogram(mRGBData, mGreenHistogram, 
-        mImageWidth, mImageHeight, 1);
-      calculateIntensityHistogram(mRGBData, mBlueHistogram, 
-        mImageWidth, mImageHeight, 2);
-
-      // Calculate mean
-      double imageRedMean = 0, imageGreenMean = 0, imageBlueMean = 0;
-      double redHistogramSum = 0, greenHistogramSum = 0, blueHistogramSum = 0;
-      for (int bin = 0; bin < 256; bin++) {
-        imageRedMean += mRedHistogram[bin] * bin;
-        redHistogramSum += mRedHistogram[bin];
-        imageGreenMean += mGreenHistogram[bin] * bin;
-        greenHistogramSum += mGreenHistogram[bin];
-        imageBlueMean += mBlueHistogram[bin] * bin;
-        blueHistogramSum += mBlueHistogram[bin];
-      }
-      imageRedMean /= redHistogramSum;
-      imageGreenMean /= greenHistogramSum;
-      imageBlueMean /= blueHistogramSum;
-
-      // Calculate second moment
-      double imageRed2ndMoment = 0, imageGreen2ndMoment = 0, imageBlue2ndMoment = 0;
-      for (int bin = 0; bin < 256; bin++) {
-        imageRed2ndMoment += mRedHistogram[bin] * mBinSquared[bin];
-        imageGreen2ndMoment += mGreenHistogram[bin] * mBinSquared[bin];
-        imageBlue2ndMoment += mBlueHistogram[bin] * mBinSquared[bin];
-      }
-      imageRed2ndMoment /= redHistogramSum;
-      imageGreen2ndMoment /= greenHistogramSum;
-      imageBlue2ndMoment /= blueHistogramSum;
-      double imageRedStdDev = Math.sqrt( imageRed2ndMoment - imageRedMean*imageRedMean );
-      double imageGreenStdDev = Math.sqrt( imageGreen2ndMoment - imageGreenMean*imageGreenMean );
-      double imageBlueStdDev = Math.sqrt( imageBlue2ndMoment - imageBlueMean*imageBlueMean );
-
-      // Draw mean
-      String imageMeanStr = "Mean (R,G,B): " + String.format("%.4g", imageRedMean) + ", " + String.format("%.4g", imageGreenMean) + ", " + String.format("%.4g", imageBlueMean);
-      canvas.drawText(imageMeanStr, marginWidth+10-1, 30-1, mPaintBlack);
-      canvas.drawText(imageMeanStr, marginWidth+10+1, 30-1, mPaintBlack);
-      canvas.drawText(imageMeanStr, marginWidth+10+1, 30+1, mPaintBlack);
+      // Draw a string
+      String imageMeanStr = "Hello World!";
+      canvas.drawText(imageMeanStr, marginWidth+10-1, 30-1, mPaintRed);
+      canvas.drawText(imageMeanStr, marginWidth+10+1, 30-1, mPaintGreen);
+      canvas.drawText(imageMeanStr, marginWidth+10+1, 30+1, mPaintBlue);
       canvas.drawText(imageMeanStr, marginWidth+10-1, 30+1, mPaintBlack);
       canvas.drawText(imageMeanStr, marginWidth+10, 30, mPaintYellow);
-
-      // Draw standard deviation
-      String imageStdDevStr = "Std Dev (R,G,B): " + String.format("%.4g", imageRedStdDev) + ", " + String.format("%.4g", imageGreenStdDev) + ", " + String.format("%.4g", imageBlueStdDev);
-      canvas.drawText(imageStdDevStr, marginWidth+10-1, 60-1, mPaintBlack);
-      canvas.drawText(imageStdDevStr, marginWidth+10+1, 60-1, mPaintBlack);
-      canvas.drawText(imageStdDevStr, marginWidth+10+1, 60+1, mPaintBlack);
-      canvas.drawText(imageStdDevStr, marginWidth+10-1, 60+1, mPaintBlack);
-      canvas.drawText(imageStdDevStr, marginWidth+10, 60, mPaintYellow);
-
-      // Draw red intensity histogram
-      float barMaxHeight = 3000;
-      float barWidth = ((float)newImageWidth) / 256;
-      float barMarginHeight = 2;
-      RectF barRect = new RectF();
-      barRect.bottom = canvasHeight - 200;
-      barRect.left = marginWidth;
-      barRect.right = barRect.left + barWidth;
-      for (int bin = 0; bin < 256; bin++) {
-        float prob = (float)mRedHistogram[bin] / (float)redHistogramSum;
-        barRect.top = barRect.bottom - 
-          Math.min(80,prob*barMaxHeight) - barMarginHeight;
-        canvas.drawRect(barRect, mPaintBlack);
-        barRect.top += barMarginHeight;
-        canvas.drawRect(barRect, mPaintRed);
-        barRect.left += barWidth;
-        barRect.right += barWidth;
-      }
-
-      // Draw green intensity histogram
-      barRect.bottom = canvasHeight - 100;
-      barRect.left = marginWidth;
-      barRect.right = barRect.left + barWidth;
-      for (int bin = 0; bin < 256; bin++) {
-        barRect.top = barRect.bottom - Math.min(80, ((float)mGreenHistogram[bin])/((float)greenHistogramSum) * barMaxHeight) - barMarginHeight;
-        canvas.drawRect(barRect, mPaintBlack);
-        barRect.top += barMarginHeight;
-        canvas.drawRect(barRect, mPaintGreen);
-        barRect.left += barWidth;
-        barRect.right += barWidth;
-      }
-
-      // Draw blue intensity histogram
-      barRect.bottom = canvasHeight;
-      barRect.left = marginWidth;
-      barRect.right = barRect.left + barWidth;
-      for (int bin = 0; bin < 256; bin++) {
-        barRect.top = barRect.bottom - Math.min(80, ((float)mBlueHistogram[bin])/((float)blueHistogramSum) * barMaxHeight) - barMarginHeight;
-        canvas.drawRect(barRect, mPaintBlack);
-        barRect.top += barMarginHeight;
-        canvas.drawRect(barRect, mPaintBlue);
-        barRect.left += barWidth;
-        barRect.right += barWidth;
-      }
-    }
+      //}
 
     // Draw our parent
     super.onDraw(canvas);
-
   }
 
-  // Decode YUV420SP colorspace to RGB
-  static public void decodeYUV420SP(int[] rgb, byte[] yuv420sp, int width, int height) {
-    // Calculate frame size
-    final int frameSize = width * height;
-
-    // Lets do this with a lot of bit twiddling for some speed
-    for (int j = 0, yp = 0; j < height; j++) {
-      int uvp = frameSize + (j >> 1) * width, u = 0, v = 0;
-      for (int i = 0; i < width; i++, yp++) {
-        // Get luminosity and set uv values
-        int y = (0xff & ((int) yuv420sp[yp])) - 16;
-        if (y < 0) y = 0;
-        if ((i & 1) == 0) {
-          v = (0xff & yuv420sp[uvp++]) - 128;
-          u = (0xff & yuv420sp[uvp++]) - 128;
-        }
-
-        // Actual converstion equations
-        int y1192 = 1192 * y;
-        int r = (y1192 + 1634 * v);
-        int g = (y1192 - 833 * v - 400 * u);
-        int b = (y1192 + 2066 * u);
-
-        // Must be within a certain range and we don't care about errors.
-        if (r < 0) r = 0; else if (r > 262143) r = 262143;
-        if (g < 0) g = 0; else if (g > 262143) g = 262143;
-        if (b < 0) b = 0; else if (b > 262143) b = 262143;
-
-        // Pack it into bytes
-        rgb[yp] = 0xff000000 | ((r << 6) & 0xff0000) | ((g >> 2) & 0xff00) | ((b >> 10) & 0xff);
-      }
-    }
-  }
-
-  static public void decodeYUV420SPGrayscale(int[] rgb, byte[] yuv420sp, int width, int height) {
-    // Calculate the frame size
-    final int frameSize = width * height;
-
-    // Lets do this with a lot of bit twiddling for some speed
-    for (int pix = 0; pix < frameSize; pix++) {
-      // Get luminosity and ignore the other components
-      int pixVal = (0xff & ((int) yuv420sp[pix])) - 16;
-      // Must be within a certain range and we don't care about errors.
-      if (pixVal < 0) pixVal = 0;
-      if (pixVal > 255) pixVal = 255;
-      // Set RGB to the same value based on luminosity
-      rgb[pix] = 0xff000000 | (pixVal << 16) | (pixVal << 8) | pixVal;
-    }
-  }
- 
-  static public void calculateIntensityHistogram(int[] rgb, int[] histogram, int width, int height, int component) {
-    // Zero histogram bins
-    for (int bin = 0; bin < 256; bin++) {
-      histogram[bin] = 0;
-    }
-
-    // RED
-    if (component == 0) {
-      for (int pix = 0; pix < width*height; pix += 3) {
-        int pixVal = (rgb[pix] >> 16) & 0xff; // bitshift and mask to get red value
-        histogram[ pixVal ]++;
-      }
-    }
-
-    // GREEN
-    else if (component == 1) {
-      for (int pix = 0; pix < width*height; pix += 3) {
-        int pixVal = (rgb[pix] >> 8) & 0xff; // bitshift and mask to get green value
-        histogram[ pixVal ]++;
-      }
-    }
-
-    // Must be BLUE
-    else {
-      for (int pix = 0; pix < width*height; pix += 3) {
-        int pixVal = rgb[pix] & 0xff; // mask for blue value
-        histogram[ pixVal ]++;
-      }
-    }
-  }
 }
--- Superpowers/src/com/adaburrows/superpowers/Superpowers.java
+++ Superpowers/src/com/adaburrows/superpowers/Superpowers.java
@@ -2,27 +2,34 @@ package com.adaburrows.superpowers;
 
 import java.io.IOException;
 
+import android.util.Log;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.RectF;
 import android.hardware.Camera;
-import android.hardware.Camera.PreviewCallback;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.view.ViewGroup.LayoutParams;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.media.MediaRecorder.AudioSource;
+import android.media.audiofx.Visualizer;
 
-public class Superpowers extends Activity {    
-  private Preview mPreview;
+public class Superpowers extends Activity {
+  private static final String TAG = "Superpowers";
+
+  private CameraView mCameraView;
+  Camera mCamera;
   private MagicOverlay mOverlay;
+  AudioRecord mAudio;
+  Visualizer mAudioVisualizer;
+  int mAudioSessionId;
+  int mAudioSampleRate;
+  int mAudioChannelFormat;
+  int mAudioEncodingFormat;
+  int mAudioBufferSize;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +38,116 @@ public class Superpowers extends Activity {
     // Hide the window title.
     getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-    mOverlay = new MagicOverlay(this);
-    mPreview = new Preview(this, mOverlay);
-    setContentView(mPreview);
-    //setContentView(R.layout.main);
-    addContentView(mOverlay, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
   }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    if (mCamera != null) {
+      mCamera.stopPreview();
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (mCamera != null) {
+      mCamera.release();
+      mCamera = null;
+    }
+    // Stop the audio subsytem
+    if (mAudio != null) {
+      mAudio.stop();
+      mAudio.release();
+      mAudio = null;
+    }
+    if (mAudioVisualizer != null) {
+      mAudioVisualizer.release();
+    }
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    mCamera = getCamera();
+    setupCamera();
+    mCameraView = new CameraView(this, mCamera);
+    setupAudio();
+    mOverlay = new MagicOverlay(this, mAudioVisualizer);
+    setContentView(mCameraView);
+    addContentView(mOverlay, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+    //setContentView(R.layout.main);
+  }
+
+  public static Camera getCamera() {
+    Camera c = null;
+    try {
+      // attempt to get a Camera instance
+      c = Camera.open();
+    }
+    catch (Exception exception){
+      // Camera is not available (in use or does not exist)
+      Log.d(TAG, "Error fetching camera: " + exception.getMessage());
+    }
+    return c;
+  }
+
+  public void setupCamera() {
+    if (mCamera != null) {
+      Camera.Parameters parameters = mCamera.getParameters();
+      /*
+       * This needs to be changed. I'm using two obsolete API calls because it was easier.
+       */
+      parameters.setPreviewSize(640, 480); //obs
+      parameters.setPreviewFrameRate(30); //obs
+      parameters.setSceneMode(Camera.Parameters.SCENE_MODE_NIGHT);
+      parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+      // Use above parameters
+      mCamera.setParameters(parameters);
+    }
+  }
+
+  public void setupAudio() {
+    //HOWTO:  Set up audio buffer for reading.
+    mAudioSampleRate = 44100;
+    mAudioChannelFormat = AudioFormat.CHANNEL_IN_MONO;
+    mAudioEncodingFormat = AudioFormat.ENCODING_PCM_16BIT;
+    mAudioBufferSize = AudioRecord.getMinBufferSize(
+      mAudioSampleRate,
+      mAudioChannelFormat,
+      mAudioEncodingFormat
+    );
+    mAudio = new AudioRecord(
+      MediaRecorder.AudioSource.MIC,
+      mAudioSampleRate,
+      mAudioChannelFormat,
+      mAudioEncodingFormat,
+      mAudioBufferSize
+    );
+    mAudioSessionId = mAudio.getAudioSessionId();
+
+    // new Thread(new Runnable(){
+    //   public void run(){
+    //     mAudio.startRecording();
+    //     while (true) {
+    //       int chunk_size = mAudioBufferSize / (40 * 2);
+    //       short[] samples = new short[40];
+    //       for (int i = 0; i < 40; i++) {
+    //         int offset = i * chunk_size;
+    //         mAudio.read(samples, offset, chunk_size);
+    //       }
+    //     }
+    //   }
+    // });
+
+    mAudioVisualizer = null;
+    try {
+      mAudioVisualizer = new Visualizer(mAudioSessionId);
+      mAudioVisualizer.setEnabled(true);
+    } catch (Exception exception) {
+      Log.d(TAG, "Error creating Visualizer: " + exception.getMessage());
+    }
+  }
+
 }
